2a2fe60b8e067ea5d148158aa705ad745f6205ce
momen khaled <momenkhaled360@gmail.com>
2026-01-29 20:40:43 +0200

    last Migrations update

--- Medical Team B/Controllers/UserProfilesController.cs
+++ Medical Team B/Controllers/UserProfilesController.cs
@@ -1,66 +1,66 @@
-﻿using MedLink.Domain.Entities.User;
-using MedLink_Application.Interfaces.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿//using MedLink.Domain.Entities.User;
+//using MedLink_Application.Interfaces.Services;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace Medical_Team_B.Controllers
-{
-    public class UserProfilesController:BaseApiController
-    {
+//namespace Medical_Team_B.Controllers
+//{
+//    public class UserProfilesController:BaseApiController
+//    {
 
-            private readonly IUserProfileService _userProfileService;
+//            private readonly IUserProfileService _userProfileService;
 
-            public UserProfilesController(IUserProfileService userProfileService)
-            {
-                _userProfileService = userProfileService;
-            }
+//            public UserProfilesController(IUserProfileService userProfileService)
+//            {
+//                _userProfileService = userProfileService;
+//            }
 
-            // GET: api/userprofiles/{id}
-            [HttpGet("{id:guid}")]
-            public async Task<IActionResult> GetById(Guid id)
-            {
-                var profile = await _userProfileService.GetByIdAsync(id);
+//            // GET: api/userprofiles/{id}
+//            [HttpGet("{id:guid}")]
+//            public async Task<IActionResult> GetById(Guid id)
+//            {
+//                var profile = await _userProfileService.GetByIdAsync(id);
 
-                if (profile == null)
-                    return NotFound();
+//                if (profile == null)
+//                    return NotFound();
 
-                return Ok(profile);
-            }
+//                return Ok(profile);
+//            }
 
-            // GET: api/userprofiles?pageIndex=1&pageSize=10
-            [HttpGet]
-            public async Task<IActionResult> GetAll(
-                [FromQuery] int pageIndex = 1,
-                [FromQuery] int pageSize = 10)
-            {
-                var profiles = await _userProfileService.GetAllAsync(pageIndex, pageSize);
-                return Ok(profiles);
-            }
+//            // GET: api/userprofiles?pageIndex=1&pageSize=10
+//            [HttpGet]
+//            public async Task<IActionResult> GetAll(
+//                [FromQuery] int pageIndex = 1,
+//                [FromQuery] int pageSize = 10)
+//            {
+//                var profiles = await _userProfileService.GetAllAsync(pageIndex, pageSize);
+//                return Ok(profiles);
+//            }
 
-            // POST: api/userprofiles
-            [HttpPost]
-            public async Task<IActionResult> Create([FromBody] UserProfile profile)
-            {
-                var createdProfile = await _userProfileService.AddAsync(profile);
+//            // POST: api/userprofiles
+//            [HttpPost]
+//            public async Task<IActionResult> Create([FromBody] UserProfile profile)
+//            {
+//                var createdProfile = await _userProfileService.AddAsync(profile);
 
-                return CreatedAtAction(
-                    nameof(GetById),
-                    new { id = createdProfile.Id },
-                    createdProfile
-                );
-            }
+//                return CreatedAtAction(
+//                    nameof(GetById),
+//                    new { id = createdProfile.Id },
+//                    createdProfile
+//                );
+//            }
 
-            // PUT: api/userprofiles/{id}
-            [HttpPut("{id:guid}")]
-            public async Task<IActionResult> Update(int id, [FromBody] UserProfile profile)
-            {
-            if (id != profile.Id)
-                return BadRequest("Id mismatch");
+//            // PUT: api/userprofiles/{id}
+//            [HttpPut("{id:guid}")]
+//            public async Task<IActionResult> Update(int id, [FromBody] UserProfile profile)
+//            {
+//            if (id != profile.Id)
+//                return BadRequest("Id mismatch");
 
-            var updatedProfile = await _userProfileService.UpdateAsync(profile);
-                return Ok(updatedProfile);
-            }
-        }
-    }
+//            var updatedProfile = await _userProfileService.UpdateAsync(profile);
+//                return Ok(updatedProfile);
+//            }
+//        }
+//    }
 
 
 
